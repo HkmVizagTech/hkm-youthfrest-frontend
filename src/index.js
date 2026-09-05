a5f3b95921e5e6894b42a9d6503c65a2d56eb974
HKM Vizag Tech <dev@hkmvizag.org>
2026-09-05 14:07:18 +0000

    Force fresh deploy to clear any stuck CDN/edge cache showing a blank page

--- src/index.js
+++ src/index.js
@@ -28,3 +28,4 @@ serviceWorker.unregister();
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
+// force-redeploy: cache refresh 1788617127
